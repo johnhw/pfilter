ef36a92fa2bce367fdb422fe5379567049aadce7
johnhw <johnhw@gmail.com>
2017-02-02 16:03:18 +0000

    Added setup.cfg

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup
  
 setup(
-     name='particlefilter',    # This is the name of your PyPI-package.
+     name='pfilter',    # This is the name of your PyPI-package.
      version='0.1',                          # Update the version number for new releases
-     packages=['pfilter']                  # The name of your scipt, and also the command you'll be using for calling it
+     packages=['pfilter'],                  # The name of your scipt, and also the command you'll be using for calling it
+     description = 'A basic particle filter',
+     author = 'John H Williamson',
+     author_email = 'johnhw@gmail.com',
+     url = 'https://github.com/johnhw/pfilter', # use the URL to the github repo
+    download_url = 'https://github.com/johnhw/pfilter/tarball/0.1',
+    keywords=["particle", "probabilistic", "stochastic", "filter", "filtering"]
  )
